591635bff6f4645cb40f86735ae8716a20a3b769
Mauro Ocorso <mauro.ocorso@etermax.com>
2017-07-07 17:13:17 -0300

    Saca el dispatchToProps de App container y agrega input para probar el state tree

--- src/scenes/App/component.js
+++ src/scenes/App/component.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
 import Home from './scenes/Home';
 import Products from './scenes/Products';
 // import restrict from '../../components/Security/Auth';
 
 // import FullScreenLayout from '../../components/commons/FullScreen';
 
-const App = ({ username }) => (
-  <div className="FullScreenLayout">
-    <p>{ username }</p>
-    <Switch>
-      <Route exact path="/home" component={Home} />
-      <Route exact path="/products" component={Products} />
-      {/*<Route exact path="/dashboard" component={restrict(Dashboard)} />*/}
-      <Route exact path="/" component={Home} />
-    </Switch>
-  </div>
-);
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onUserRegister(this.userInput.value);
+  }
+
+  render() {
+    return (
+      <div className="FullScreenLayout">
+        <Nav />
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" ref={ref => (this.userInput = ref)} />
+        </form>
+        <Switch>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/products" component={Products} />
+          {/*<Route exact path="/dashboard" component={restrict(Dashboard)} />*/}
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
+    );
+  }
+}
 
 App.propTypes = {
-  username: PropTypes.string.isRequired
+  onUserRegister: PropTypes.func.isRequired
 };
 
 export default App;
--- src/scenes/App/container.js
+++ src/scenes/App/container.js
@@ -2,16 +2,12 @@ import { connect } from 'react-redux';
 import App from './component';
 import { setUser } from './actions';
 
-const mapStateToProps = state => ({
-  username: state.user.username || 'NN'
-});
-
 const mapDispatchToProps = dispatch => ({
   onUserRegister: username => dispatch(setUser({ username }))
 });
 
 const AppContainer = connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps
 )(App);
 
