75025c8647e3bafd0b8152f2ece3419e94397841
Mauro Lionel Ocorso <ocorsomauro@gmail.com>
2017-07-08 14:27:48 -0300

    Agrega injectGlobal de styled-components

--- src/components/App/component.js
+++ src/components/App/component.js
@@ -1,44 +1,33 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import styled, { injectGlobal } from 'styled-components';
 import Header from './components/Header';
 import Home from '../../scenes/Home';
 import Products from '../../scenes/Products';
 // import restrict from '../../components/Security/Auth';
 
-// import FullScreenLayout from '../../components/commons/FullScreen';
-
-class App extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
+/* eslint-disable no-unused-expressions */
+injectGlobal`
+  body {
+    margin: 0;
   }
+`;
+/* eslint-enable no-unused-expressions */
 
-  handleSubmit(event) {
-    event.preventDefault();
-    this.props.onUserRegister(this.userInput.value);
-  }
+const StyledApp = styled.main`
+  font-family: Arial, sans-serif;
+`;
 
-  render() {
-    return (
-      <div className="FullScreenLayout">
-        <Header />
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" ref={ref => (this.userInput = ref)} />
-        </form>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/productos" component={Products} />
-          {/* <Route exact path="/dashboard" component={restrict(Dashboard)} /> */}
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </div>
-    );
-  }
-}
-
-App.propTypes = {
-  onUserRegister: PropTypes.func.isRequired
-};
+const App = () => (
+  <StyledApp>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/productos" component={Products} />
+      {/* <Route exact path="/dashboard" component={restrict(Dashboard)} /> */}
+      <Route exact path="/" component={Home} />
+    </Switch>
+  </StyledApp>
+);
 
 export default App;
