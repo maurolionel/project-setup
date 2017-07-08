1ab47bdbcaf57939a7c8c61896058b99f8361c29
Mauro Lionel Ocorso <ocorsomauro@gmail.com>
2017-07-08 17:35:46 -0300

    Styling básico TopBar

--- src/components/App/components/Header/components/TopBar/component.js
+++ src/components/App/components/Header/components/TopBar/component.js
@@ -3,12 +3,43 @@ import styled from 'styled-components';
 
 const StyledTopBar = styled.div`
   display: flex;
-  background-color: blue;
-  color: #fff;
+  justify-content: space-between;
+  background-color: ${props => props.theme.primary};
+`;
+
+const TopBarSection = styled.div`
+  display: flex;
+`;
+
+const TobBarLink = styled.a.attrs({
+  href: '#'
+})`
+  padding: 0.7rem 1rem;
+  color: ${props => props.theme.base};
+  text-decoration: none;
+  text-transform: uppercase;
+
+  &:hover {
+    background-color: blue;
+  }
+  &:active {
+    color: orange;
+    background-color: blue;
+  }
 `;
 
 const TopBar = () => (
-  <StyledTopBar>TOPBAR</StyledTopBar>
+  <StyledTopBar>
+    <TopBarSection>
+      <TobBarLink>Horarios</TobBarLink>
+      <TobBarLink>Métodos de envío</TobBarLink>
+      <TobBarLink>Teléfono: (011) 3220.0500</TobBarLink>
+    </TopBarSection>
+    <TopBarSection>
+      <TobBarLink>Entrar</TobBarLink>
+      <TobBarLink>Carrito</TobBarLink>
+    </TopBarSection>
+  </StyledTopBar>
 );
 
 export default TopBar;
