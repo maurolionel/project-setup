24b511d71061d61043fe270f92685ab9b1830f6c
Mauro Lionel Ocorso <ocorsomauro@gmail.com>
2017-07-11 02:12:09 -0300

    Agrega CardsCarousel a Home

--- src/scenes/Home/component.js
+++ src/scenes/Home/component.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import CardsCarousel from '../../components/CardsCarousel';
 import Shortcuts from '../../components/Shortcuts';
 import Features from '../../components/Features';
 
 const Home = () => (
   <section>
     <div>--Banner--</div>
-    <div>--Slider--</div>
+    <CardsCarousel />
     <Shortcuts />
     <Features />
   </section>
