72e8fd04d98f7ab8e6a019fe7c3f607faf809d9b
Mauro Ocorso <ocorsomauro@gmail.com>
2017-07-16 11:44:48 -0300

    Styling Filter

--- src/components/Filter/component.js
+++ src/components/Filter/component.js
@@ -5,14 +5,40 @@ import styled from 'styled-components';
 const StyledFilter = styled.div``;
 
 const Title = styled.h3`
+  margin: 0.5rem 0;
   color: ${({ theme }) => theme.accent};
+  font-size: 1rem;
+  text-transform: capitalize;
+`;
+
+const Value = styled.div`
+  margin: 0.2rem;
+`;
+
+const Checkbox = styled.input.attrs({
+  type: 'checkbox',
+  id: props => props.id,
+  name: props => props.name
+})`
+  margin-right: 0.5rem
+`;
+
+const Label = styled.label`
+  color: ${({ theme }) => theme.gray};
   text-transform: capitalize;
 `;
 
 const Filter = ({ type, values }) => (
   <StyledFilter>
     <Title>{type}</Title>
-    {values.map((aValue, index) => <div key={index}>{aValue}</div>)}
+    <div>
+      {values.map((aValue, index) => (
+        <Value>
+          <Checkbox key={index} id={aValue} name={index} />
+          <Label htmlFor={aValue}>{aValue}</Label>
+        </Value>
+      ))}
+    </div>
   </StyledFilter>
 );
 
