1871812af30446a361bcf940dde9093abc9e1e10
Mauro Lionel Ocorso <ocorsomauro@gmail.com>
2017-07-18 20:07:51 -0300

    Convierte Button a un styled-component y agrega :active state style

--- src/components/Button/component.js
+++ src/components/Button/component.js
@@ -1,35 +1,20 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import { darken } from 'polished';
 import Paper from '../Paper';
 
-const StyledButton = styled(Link)`
+const PaperLink = Paper.withComponent(Link);
+
+const Button = PaperLink.extend`
   display: inline-block;
   padding: 0.5rem 0.8rem;
   color: ${({ primary, theme }) => (primary ? theme.base : theme.gray)};
   background-color: ${({ primary, theme }) => (primary ? theme.accent : theme.whiteGray)};
   text-decoration: none;
+
+  &:active {
+    color: ${({ primary, theme }) => darken(0.05, primary ? theme.base : theme.gray)};
+    background-color: ${({ primary, theme }) => darken(0.05, primary ? theme.accent : theme.whiteGray)};
+  }
 `;
-
-const Button = ({ primary = false, to = './', children = '' }) => (
-  <Paper>
-    <StyledButton to={to} primary={primary}>
-      {children}
-    </StyledButton>
-  </Paper>
-);
-
-Button.propTypes = {
-  primary: PropTypes.bool,
-  to: PropTypes.string,
-  children: PropTypes.node
-};
-
-Button.defaultProps = {
-  primary: false,
-  to: './',
-  children: ''
-};
 
 export default Button;
