c449332c8aa10b120042afbf33e35ff0e110474c
Mauro Lionel Ocorso <ocorsomauro@gmail.com>
2017-07-19 01:23:08 -0300

    Personaliza Input

--- src/components/Input/component.js
+++ src/components/Input/component.js
@@ -1,5 +1,18 @@
+import { darken } from 'polished';
 import Paper from '../Paper';
 
-const Input = Paper.withComponent('input');
+const StyledInput = Paper.withComponent('input');
+
+const Input = StyledInput.extend`
+  padding: 0.5rem 0.8rem;
+  border: 0;
+  outline: 0;
+  color: ${({ type, theme }) => (type === 'submit' ? theme.base : darken(0.5, theme.grayLighter))};
+  background-color: ${({ type, theme }) => (type === 'submit' ? theme.accent : theme.base)};
+
+  &:active {
+    background-color: ${({ type, theme }) => (type === 'submit' ? darken(0.05, theme.accent) : theme.base)};
+  }
+`;
 
 export default Input;
