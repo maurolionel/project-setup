acd9f853ef9c5037c3efff4e01f24ab03f6b07f7
Mauro Ocorso <mauro.ocorso@etermax.com>
2017-07-19 10:31:19 -0300

    Convierte NavLinks de TopBar en Links a Contacto y elimina esas scenes en desuso

--- src/components/App/components/Header/components/TopBar/component.js
+++ src/components/App/components/Header/components/TopBar/component.js
@@ -20,25 +20,25 @@ const TopBarLink = TopBarNavLink.withComponent(Link);
 const TopBar = () => (
   <StyledTopBar>
     <TopBarSection>
-      <TopBarNavLink to="/horarios" title="Nuestros horarios de atención">
+      <TopBarLink to="/contacto" title="Conocé nuestros horarios de atención.">
         <Icon className="fa fa-clock-o" />
         Horarios
-      </TopBarNavLink>
-      <TopBarNavLink to="/envios" title="Conocé nuestros métodos de envío">
+      </TopBarLink>
+      <TopBarLink to="/contacto" title="Conocé nuestros métodos de envío.">
         <Icon className="fa fa-truck" />
         Métodos de envío
-      </TopBarNavLink>
+      </TopBarLink>
       <TopBarLink to="/contacto" title="Llamanos, ¡tu consulta no molesta!">
         <Icon className="fa fa-phone" />
         Teléfono: (011) 3220.0500
       </TopBarLink>
     </TopBarSection>
     <TopBarSection>
-      <TopBarNavLink to="/ingresar" title="Ingresá a tu cuenta">
+      <TopBarNavLink to="/ingresar" title="Ingresá a tu cuenta.">
         Ingresar
         <Icon className="fa fa-sign-in" placement="right" />
       </TopBarNavLink>
-      <TopBarNavLink to="/carrito" title="Tu carrito de compras">
+      <TopBarNavLink to="/carrito" title="Tu carrito de compras.">
         <Icon className="fa fa-shopping-cart" />
       </TopBarNavLink>
     </TopBarSection>
--- src/scenes/Schedules/component.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Schedules = () => <h1>Horarios de atención</h1>;
-
-export default Schedules;
