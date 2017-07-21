612bdfbf291a4254e2d9d130ed2c12fea02c75ad
Mauro Ocorso <mauro.ocorso@etermax.com>
2017-07-21 10:16:38 -0300

    Parametriza Paper

--- src/components/Paper/component.js
+++ src/components/Paper/component.js
@@ -1,16 +1,20 @@
 import styled from 'styled-components';
 
+const boxShadowBase = '0 1px 1px 0 rgba(0, 0, 0, 0.1), 0 -1px 2px 0 rgba(0, 0, 0, 0.1)';
+const boxShadowWithStateChanged = '0 1px 1px 0 rgba(0, 0, 0, 0.2), 0 -1px 3px 1px rgba(0, 0, 0, 0.1)';
+
 const Paper = styled.div`
   display: inline-flex;
   overflow: hidden;
   border-radius: 3px;
-  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.1), 0 -1px 2px 0 rgba(0, 0, 0, 0.1);
+  box-shadow: ${boxShadowBase};
   transition-duration: 0.25s, 0.25s;
   transition-property: box-shadow, box-shadow;
   transition-timing-function: ease, ease;
 
   &:hover, &:focus {
-    box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.2), 0 -1px 3px 1px rgba(0, 0, 0, 0.1);
+    box-shadow: 
+      ${({ withoutChangingStateStyle }) => (withoutChangingStateStyle ? boxShadowBase : boxShadowWithStateChanged)};
   }
 `;
 
