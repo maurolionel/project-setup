38acfc4a58d260252f1c6328d5672b343279f041
Mauro Ocorso <mauro.ocorso@etermax.com>
2017-07-24 09:46:34 -0300

    Reubica componentes de Home

--- src/scenes/Home/component.js
+++ src/scenes/Home/component.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Banner from './components/Banner';
-import CardsCarousel from '../../components/CardsCarousel';
-import Shortcuts from '../../components/Shortcuts';
-import Features from '../../components/Features';
+import CardsCarousel from './components/CardsCarousel';
+import Shortcuts from './components/Shortcuts';
+import Features from './components/Features';
 
 const Home = () => (
   <section>
