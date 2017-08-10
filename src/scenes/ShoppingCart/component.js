6c562e842edc75d13786b885501db412d119068b
Mauro Lionel Ocorso <ocorsomauro@gmail.com>
2017-08-10 00:19:57 -0300

    Scene ShoppingCart

--- src/scenes/ShoppingCart/component.js
+++ src/scenes/ShoppingCart/component.js
@@ -1,28 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Anchor from '../../components/Anchor';
 
 const ShoppingCart = ({ productsInCart, onIncreaseQuantity, onDecreaseQuantity, onRemoveFromCart }) => (
-  <div>
-    <ul>
-      {productsInCart.map((aProductInCart, index) => (
-        <li key={aProductInCart.id}>
-          <ul>
-            <li>{index}</li>
-            <li>{aProductInCart.name}</li>
-            <li>{aProductInCart.price}</li>
-            <li>
-              <button onClick={() => onDecreaseQuantity(aProductInCart.id)}>-</button>
-              <span>{aProductInCart.quantity}</span>
-              <button onClick={() => onIncreaseQuantity(aProductInCart.id)}>+</button>
-            </li>
-            <li>
-              <button onClick={() => onRemoveFromCart(aProductInCart.id)}>X</button>
-            </li>
-          </ul>
-        </li>
-      ))}
-    </ul>
-  </div>
+  productsInCart.length > 0
+    ? (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="6">Productos: {productsInCart.length}</th>
+            </tr>
+            <tr>
+              <th>#</th>
+              <th>Producto</th>
+              <th>Precio unitario</th>
+              <th>Cantidad</th>
+              <th>Precio total</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {productsInCart.map((aProductInCart, index) => (
+              <tr key={aProductInCart.id}>
+                <td>{index + 1}</td>
+                <td>{aProductInCart.name}</td>
+                <td>$ {aProductInCart.price}</td>
+                <td>
+                  <button onClick={() => onDecreaseQuantity(aProductInCart.id)}>-</button>
+                  <span>{aProductInCart.quantity}</span>
+                  <button onClick={() => onIncreaseQuantity(aProductInCart.id)}>+</button>
+                </td>
+                <td>$ {aProductInCart.price * aProductInCart.quantity}</td>
+                <td>
+                  <button onClick={() => onRemoveFromCart(aProductInCart.id)}>X</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan="3" />
+              <td>Total</td>
+              <td>$...</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    )
+    : (
+      <div>
+        <p>Todavía no tenés ningún producto en el carrito.</p>
+        <p>Podés hacerlo desde la sección <Anchor to="/productos">productos</Anchor>.</p>
+      </div>
+    )
 );
 
 ShoppingCart.propTypes = {
