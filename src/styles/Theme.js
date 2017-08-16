7538e6f6c93a92784c57db5eeb3775f0a71fe327
Mauro Lionel Ocorso <ocorsomauro@gmail.com>
2017-08-15 22:22:05 -0300

    Elimina Theme en desuso

--- src/styles/Theme.js
+++
@@ -1,8 +0,0 @@
-import createTheme from 'styled-components-theme';
-import colors from './Colors';
-
-const theme = createTheme(...Object.keys(colors));
-
-export const themeColors = colors;
-
-export default theme;
