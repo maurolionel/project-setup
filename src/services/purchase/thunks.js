ab92ac793c3cfc2536349ecc73d5a89428efdd15
Mauro Lionel Ocorso <ocorsomauro@gmail.com>
2017-09-09 19:30:09 -0300

    BugFix purchase thunk

--- src/services/purchase/thunks.js
+++ src/services/purchase/thunks.js
@@ -3,15 +3,18 @@ import { createPreferenceRequest, createPreferenceSuccess } from './actions';
 import appConfig from '../../config';
 
 /* eslint-disable import/prefer-default-export */
-export const createPreference = items => ({
-  [CALL_API]: {
-    endpoint: `${appConfig.api.path}payments/create-preference/`,
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify(items),
-    types: [createPreferenceRequest, createPreferenceSuccess, 'FAILURE']
-  }
-});
+export const createPreference = () => (dispatch, getState) => {
+  const { shoppingCart: { all: items } } = getState();
+  dispatch({
+    [CALL_API]: {
+      endpoint: `${appConfig.api.path}payments/create-preference/`,
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(items),
+      types: [createPreferenceRequest, createPreferenceSuccess, 'FAILURE']
+    }
+  });
+};
