9a2623ce74d659f78ed5fb236c8b1ee95edefa11
Mauro Ocorso <mauro.ocorso@etermax.com>
2017-11-30 11:10:27 -0300

    Agrega el email del usuario al modal que se muestra al finalizar la compra

--- src/components/ModalPurchase/component.js
+++ src/components/ModalPurchase/component.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Modal from '../Modal';
-import Title from '../Title';
 import Button from '../Button';
 
-const ModalPayment = ({ isOpen, onPurchaseEnd }) => isOpen && (
-  <Modal isDismissible={false}>
-    <Title>¡Gracias por su compra!</Title>
-    <p>grax crack!</p>
-    <Button onClick={onPurchaseEnd} primary>Finalizar</Button>
+const Wrapper = styled.div`
+  padding: 0 20px 15px;
+`;
+
+const Email = styled.p`
+  font-weight: 700;
+`;
+
+const ModalPayment = ({ email, isOpen, onPurchaseEnd }) => isOpen && (
+  <Modal title="¡Gracias por su compra!" isDismissible={false}>
+    <Wrapper>
+      <p>Te envíamos un mail a</p>
+      <Email>{email}</Email>
+      <p>con los datos necesarios para concretar la compra.</p>
+      <Button onClick={onPurchaseEnd} primary>Finalizar</Button>
+    </Wrapper>
   </Modal>
 );
 
 ModalPayment.propTypes = {
+  email: PropTypes.string.isRequired,
   isOpen: PropTypes.bool.isRequired,
   onPurchaseEnd: PropTypes.func.isRequired
 };
