0b4aca98d54476eab055cef9ac775e95ff1ccfe6
Mauro Lionel Ocorso <ocorsomauro@gmail.com>
2017-12-11 21:27:10 -0300

    Agrega path local de api system

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,8 @@ const appConfig = {
   version: '__version__',
   api: {
     // path: 'http://hernanlevinton.com/imprek/api/public/'
-    path: 'http://localhost/projects/imprek-ecommerce/api/public/'
+    path: 'http://localhost/projects/imprek-ecommerce/api/public/',
+    imprek: 'http://localhost/projects/api-system/public/'
   }
 };
 
