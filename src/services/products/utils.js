44ab3f22b9631eb78b93c11e759108c042496e4e
Mauro Lionel Ocorso <ocorsomauro@gmail.com>
2017-12-12 02:46:21 -0300

    WIP: Normalize prices en products

--- src/services/products/utils.js
+++ src/services/products/utils.js
@@ -7,12 +7,22 @@ export const initialState = {
   visibilityFilter: 0
 };
 
+export const normalizePrices = (accumulator, nextValue) => {
+  console.log(accumulator);
+  return {
+    ...accumulator,
+    [Object.keys(nextValue)]: {
+      id: nextValue.id
+    }
+  };
+};
+
 export const mapProductFromApiToState = aProduct => ({
   id: convertStringToNumber(aProduct.id),
   categoryId: convertStringToNumber(aProduct.categoryId),
   brandId: convertStringToNumber(aProduct.brandId),
   name: aProduct.name,
-  prices: aProduct.prices,
+  prices: aProduct.prices.reduce(normalizePrices, {}),
   images: aProduct.images,
   description: aProduct.description || 'Sin descripción',
   hasStock: Boolean(convertStringToNumber(aProduct.hasStock))
