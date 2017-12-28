764f2ec9a73cc80f8e6bc27d34f3c0ba9d03405b
Mauro Ocorso <ocorsomauro@gmail.com>
2017-12-28 05:43:13 -0300

    wip faqs

---
+++ src/services/faqs/qa.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: 1,
+    question: '¿Qué es un sistema continuo?',
+    answer: 'El sistema continuo es un sistema de depósitos recargables de tinta que te permite controlar los niveles de tinta y se rellena fácilmente.' +
+    '<br />El mismo cumple la función de recargar de forma constante los cartuchos de su impresora, manteniendo los cartuchos siempre llenos.' +
+    'El sistema se conforma por cartuchos, mangueras y tanques. La tinta se encuentra en los tanques que externos y están conectados a los cartuchos a través de mangueras. Cuando la impresora reconoce que el cartucho está vacío, el chip cumple la función de reseteo diciéndole a la máquina que se instaló un cartucho nuevo.'
+  },
+  {
+    id: 2,
+    question: '¿Cuáles son las ventajas del sistema continuo?',
+    answer: `1. Se puede imprimir ininterrumpidamente cientos de hojas.
+    2. Se reduce drásticamente el cambio de cartuchos.
+    3. Un ahorro de hasta el 90 % en cambio de cartuchos.
+    4. Se puede recargar los tanques externos cuantas veces se necesite durante la vida útil del cartucho`
+  }
+];
--- src/services/faqs/reducer.js
+++ src/services/faqs/reducer.js
@@ -1,16 +1,7 @@
+import qa from './qa';
+
 const initialState = {
-  all: [
-    {
-      id: 1,
-      question: '¿Qué es un sistema continuo o CISS?',
-      answer: 'Un sistema continuo o CISS (Continuos Ink Suplier System) es un bla bla bla ble ble ble...'
-    },
-    {
-      id: 2,
-      question: '¿Qué es un sistema continuo o CISS?',
-      answer: 'Un sistema continuo o CISS (Continuos Ink Suplier System) es un bla bla bla ble ble ble...'
-    }
-  ]
+  all: qa
 };
 
 function faqs(state = initialState, action = {}) {
